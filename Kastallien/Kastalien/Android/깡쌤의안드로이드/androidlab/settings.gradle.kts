d22c8ad8ebcd916c1982a6bf31b31ede8475c140
icecrao2 <icecrao2@daum.net>
2025-12-08 09:26:39 +0900

    new git

---
+++ Kastallien/Kastalien/Android/깡쌤의안드로이드/androidlab/settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AndroidLab"
+include(":app")
+include(":ch6_view")
+include(":ch7_layout")
+include(":ch8_event")
+include(":ch11_jetpack")
